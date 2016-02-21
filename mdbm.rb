f2d84a0ba417ccb502969ec31a67be94811ac686
Evan Miller <evan@squareup.com>
2016-02-21 08:55:18 -0800

    x

--- mdbm.rb
+++ mdbm.rb
@@ -1,7 +1,7 @@
 class Mdbm < Formula
   desc "super-fast memory-mapped key/value store."
   homepage "http://yahooeng.tumblr.com/post/104861108931/mdbm-high-speed-database"
-  url "https://github.com/yahoo/mdbm/archive/v4.12.3.tar.gz"
+  url "https://github.com/yahoo/mdbm/archive/v4.12.3.tar.gz", :fork_ok => true
   sha256 "1bdd27696980b8234893f2c7bfbd0d1ad5c06ccdb1eb91bcf053db27c61eea26"
 
   depends_on "coreutils"
